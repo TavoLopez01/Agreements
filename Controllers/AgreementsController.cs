7953ef1bea7ab5056983762fa2caa4ddea3dde8a
Ospina Lopez, Gustavo Adolfo, Enel Colombia Extern <gaospina@indracompany.com>
2021-02-27 17:59:25 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/AgreementsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Agreements.Data;
+using Agreements.Models;
+
+namespace Agreements.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AgreementsController : ControllerBase
+    {
+        private readonly AgreementsData _agreementsData;
+
+        public AgreementsController(AgreementsData agreementsData)
+        {
+            this._agreementsData = agreementsData ?? throw new ArgumentException(nameof(agreementsData));
+        }
+
+        [HttpGet]
+        public async Task<List<AgreementsModel>> get()
+        {
+            return await _agreementsData.GetAll();
+        }
+
+       
+    }
+}
---
+++ Data/AgreementsData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Agreements.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Agreements.Data
+{
+    public class AgreementsData
+    {
+
+        private readonly string _connection;
+
+        public AgreementsData(IConfiguration configuration)
+        {
+            _connection = configuration.GetConnectionString("defaultConnection");
+        }
+
+        public async Task<List<AgreementsModel>> GetAll()
+        {
+
+            using (SqlConnection sql = new SqlConnection(_connection))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetAllAgreements", sql))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    var response = new List<AgreementsModel>();
+                    await sql.OpenAsync();
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            AgreementsModel agreement = GetRow(reader);
+                            Console.WriteLine(agreement);
+                            response.Add(agreement);
+                        }
+                    }
+
+                    return response;
+                }
+            }
+        }
+
+        private AgreementsModel GetRow(SqlDataReader dataReader)
+        {
+            return new AgreementsModel()
+            {
+                Id = dataReader.GetInt32(0),
+                Name = dataReader.GetString(1),
+                Description = dataReader.GetString(2),
+                Amount = dataReader.GetInt32(3)
+            };
+        }
+    }
+}
